e4ae0779dc151eb4ffe1ac1dd1a2391f119b31e6
AlexandrTolstov <alextav_85@mail.ru>
2019-01-06 12:32:47 +0600

    Domain

--- DomainProgramm/Program.cs
+++ DomainProgramm/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace DomainProgramm
 {
@@ -10,6 +11,17 @@ namespace DomainProgramm
     {
         static void Main(string[] args)
         {
+            AppDomain domain = AppDomain.CurrentDomain;
+            Console.WriteLine("Name: {0}", domain.FriendlyName);
+            Console.WriteLine("Base Directory: {0}", domain.BaseDirectory);
+            Console.WriteLine();
+
+            Assembly[] assemblies = domain.GetAssemblies();
+            foreach (Assembly asm in assemblies)
+            {
+                Console.WriteLine(asm.GetName().Name);
+            }
+            Console.Read();
         }
     }
 }
